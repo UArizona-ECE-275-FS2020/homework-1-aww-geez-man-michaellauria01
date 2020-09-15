d38ebc6e77d72dd3d5384417ef2889101c8726a4
Michael Lauria <51801184+michaellauria01@users.noreply.github.com>
2020-09-15 10:35:56 -0700

    Add files via upload

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,48 +1,52 @@
+#include "Morty.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
 /*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
-| |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
-| | | | ___  _ __ ___   _____      _____  _ __| | __ / |
-| |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
-|  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
-|_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
-
-One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
-exists a Morty who says everything backwards.
-
+Author: Michael Lauria
+Date created : 09 / 13 / 20
 */
 
-// Include the Morty header file
-#include "Morty.hpp"
+using namespace std;
+using namespace C137;
+using namespace Z286;
 
+int main(int ac, char** av) {
 
-int main (int ac, char** av) {
-	// Parse the command line arguments. The program is executed as
-	// ./AwwGeezMan {start} {stop} {dimension}
-	// or 
-	// ./AwwGeezMan {start} {stop} {step} {dimension}
-	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
+	int id = 4, start, stop, step = 0;
+
+	if (ac != 6 and ac != 5) {
+		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)"
 			<< std::endl;
 		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
 		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
+
 		return -1;
 	}
-	
-	// Parse the command line arguments
-	
-		
-	// Depending on the dimension of the arguments, call the appropriate Morty
-	// function
-	
-	
+
+	start = atoi(av[2]);
+	stop = atoi(av[3]);
+	if (ac == 6) {
+		step = atoi(av[4]);
+		id = 5;
+	}
+	char* dim = av[id];
+
+	printf("Morty %s says:\n", dim);
+
+	if (strcmp(dim, "C137") == 0) {
+		if (step == 0)
+		C137::Morty(start, stop);
+		else 
+		C137::Morty(start, stop, step);
+	}
+	else if (strcmp(dim, "Z286") == 0) {
+		if (step == 0)
+		Z286::Morty(start, stop);
+		else 
+		Z286::Morty(start, stop, step);
+	}
+
 	return 0;
 }
--- src/Morty.cpp
+++ src/Morty.cpp
@@ -1,21 +1,92 @@
-/* 
- __  __            _                                
-|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \ 
-| |  | | (_) | |  | |_| |_| |  _  | (__| |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/ 
-                       |___/           |_|   |_|  
-*/
-
-// Include the Morty header file
+#include <string> 
 #include "Morty.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+using namespace C137;
+using namespace Z286;
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace C137 here 
-// =========================================================================== // 
+// =========================================================================== //
 
+namespace C137 {
+	void Morty(int start, int stop, int step) {
+		for (start; start <= stop; start = start + step) {
+			printf("%d: ", start);
 
+			if (start % 15 == 0) {
+				printf("Aww Geez Man");
+			}
+			else if (start % 3 == 0) {
+				printf("Aww");
+			}
+			else if (start % 5 == 0) {
+				printf("Geez");
+			}
+			printf("\n");
+		}
+
+	}
+
+	void Morty(int start, int stop) {
+		for (start; start <= stop; start++) {
+			printf("%d: ", start);
+
+			if (start % 15 == 0) {
+				printf("Aww Geez Man");
+			}
+			else if (start % 3 == 0) {
+				printf("Aww");
+			}
+			else if (start % 5 == 0) {
+				printf("Geez");
+			}
+			printf("\n");
+		}
+	}
+}
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
+
+
+namespace Z286 {
+	void Morty(int start, int stop, int step) {
+		for (start; start <= stop; start = start + step) {
+			printf("%d: ", start);
+
+			if (start % 15 == 0) {
+				printf("naM zeeG wwA");
+			}
+			else if (start % 3 == 0) {
+				printf("wwA");
+			}
+			else if (start % 5 == 0) {
+				printf("zeeG");
+			}
+			printf("\n");
+		}
+
+	}
+
+	void Morty(int start, int stop) {
+		for (start; start <= stop; start++) {
+			printf("%d: ", start);
+
+			if (start % 15 == 0) {
+				printf("naM zeeG wwA");
+			}
+			else if (start % 3 == 0) {
+				printf("wwA");
+			}
+			else if (start % 5 == 0) {
+				printf("zeeG");
+			}
+			printf("\n");
+		}
+	}
+}
